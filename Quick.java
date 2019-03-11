db770d0765638086aee691aae6d75d8cda54df72
Jude Rizzo <jrizzo00@stuy.edu>
2019-03-11 11:41:40 -0400

    working in class

---
+++ Quick.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+
+public class Quick{
+  private static void swap(int[] data, int i, int j){
+    int k = data[i];
+    data[i] = data[j];
+    data[j] = k;
+  }
+  public static int partition(int[] data, int start, int end){
+    int n = (int)Math.random()*(end - start)+ start;
+    swap(data, start, n);
+    start++;
+    while(end > start){
+      if(data[start] > data[n]){
+        swap(data, start, end);
+        end--;
+      }else{
+        if(data[start] < data[n]){
+          swap(data, n, start);
+          start++;
+          n++;
+
+        } else{
+          start++;
+        }
+      }
+    }
+    return n;
+  }
+
+
+
+}
