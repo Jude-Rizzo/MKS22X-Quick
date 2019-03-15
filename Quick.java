243c3daf6b8e65a8e64afd7b6d5586502f9778fa
Jude Rizzo <jrizzo00@stuy.edu>
2019-03-14 23:05:01 -0400

    done

--- Quick.java
+++ Quick.java
@@ -7,47 +7,54 @@ public class Quick{
     data[i] = data[j];
     data[j] = k;
   }
-  public static int partition(int[] data, int start, int end){
-    int n = (int)Math.random()*(end - start)+ start;
-    swap(data, start, n);
-    int pivot = start;
-    int dupcounter = 0;
-    //make an index at the beginning for the pivot point
-    start++;
-    while(end != start){
-      if(data[start] > data[pivot]){
-        if(data[end] < data[pivot]){
-          swap(data, end, start);
-          start++;
-        }
-        end--;
-      } else {
+    public static int partition(int[] data, int start, int end){
+      int pivot = (int)(Math.random() * (end - start + 1)) + start;
+      //swap pivot to the first number of index
+      if (pivot != start){
+        swap(data,start,pivot);
+        pivot = start;
         start++;
+      }
+      while (start != end){
 
+        if (data[start] > data[pivot]){
+          swap(data,start, end);
+          end--;
+        }
+
+        else{
+          start++;
+
+        }
+      }
+      //swap pivot with start if pivot is less than
+      if (data[start] <= data[pivot]) {
+       swap(data,pivot,start);
+       pivot = start;
+      }
+      else {
+       swap(data,pivot,start-1);
+       pivot = start-1;
+      }
+  
+      return pivot;
     }
-
-  }
-  return start;
-}
-
 
 public static int quickselect(int[] data, int k){
   int lo = 0;
   int hi = data.length - 1;
-  
+
   int pivot = partition(data,lo,hi);
   while(k != pivot) {
-    //if k is less than pivot look from beginning to the pivot
+
     if (k < pivot){
       hi = pivot-1;
     }
-    //else start at the pivot and go to the hi
+
     if (k > pivot){
       lo = pivot+1;
     }
     pivot = partition(data,lo,hi);
-    //System.out.println("Index: " + idx + " k: " + k + " lo: " + lo + " hi:" + hi);
-    //System.out.println("toString: " + Arrays.toString(data));
   }
   return data[k];
 }
@@ -62,6 +69,13 @@ public static int quickselect(int[] data, int k){
     quicksort(data, low, p -1);
     quicksort(data, p + 1, hi);
   }
+
+  public static void quicksort(int[] data){
+    quicksort(data,0,data.length - 1);
+  }
+
+
+
 
 
 }
